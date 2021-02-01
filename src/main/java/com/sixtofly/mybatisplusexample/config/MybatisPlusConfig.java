023e77a2abca7d2b50021e0c918d6e4a9528431f
xx <369889186@qq.com>
2021-02-01 10:56:53 +0800

    字段自动填充, 分页插件

--- src/main/java/com/sixtofly/mybatisplusexample/config/MybatisPlusConfig.java
+++ src/main/java/com/sixtofly/mybatisplusexample/config/MybatisPlusConfig.java
@@ -1,10 +1,12 @@
 package com.sixtofly.mybatisplusexample.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import com.github.pagehelper.PageInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,9 +32,17 @@ public class MybatisPlusConfig {
 //        return paginationInterceptor;
 //    }
 
+    /**
+     * pagehelper的分页插件
+     */
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        return new PageInterceptor();
+    }
 
     /**
      * 注册分页插件
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
@@ -49,6 +59,11 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(pageInterceptor);
 
         return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 
 }
